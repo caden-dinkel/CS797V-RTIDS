9e09bb2712cd7b40d21371393d4bd6b68d1d37a7
caden-dinkel <cjdinkel@proton.me>
2026-03-31 22:05:26 -0500

    afsd

---
+++ RTIDS/src/data.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('../../data/CIC-DDoS2019/DrDoS_DNS.csv')
+
+print(df.columns)
+
---
+++ RTIDS/src/model.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ["KERAS_BACKEND"] = "torch"
+
+import torch
+import keras
+import numpy as np
+

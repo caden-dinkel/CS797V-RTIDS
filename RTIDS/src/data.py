d9135a2add86c19e08f27c23e4b4739328e563d3
caden-dinkel <cjdinkel@proton.me>
2026-04-02 11:51:29 -0500

    free

--- RTIDS/src/data.py
+++ RTIDS/src/data.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('../../data/CIC-DDoS2019/DrDoS_DNS.csv')
+df = pd.read_csv('data/CIC-DDoS2019/01-12/DrDoS_DNS.csv')
+
+
+
+
 
 print(df.columns)
 
